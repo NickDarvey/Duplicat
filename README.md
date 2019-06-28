7ec7522e1549fbd031dbdf36c3a2b8c01fcfa6d6
Nick Darvey <nickdarvey@outlook.com.au>
2019-06-28 22:51:41 +1000

    Clean-up, add to-dos

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## Getting started
 
 ## TODO
+* Parse arguments in CLI.
+* Find a non-recursive solution to contents comparison.
+* Consider caching of stream values.
--- Source/Duplicat/DuplicateFinder.cs
+++ Source/Duplicat/DuplicateFinder.cs
@@ -12,8 +12,6 @@ namespace Duplicat
         public DuplicateFinder(Func<string, Stream> openFile) =>
             _openFile = openFile;
 
-        // what about zero length files?
-
         public IEnumerable<IEnumerable<string>> Find(IEnumerable<(string Path, long Size)> files) =>
             from file in files
             group file by file.Size into sizeDuplicates // TODO: https://stackoverflow.com/a/3433635
@@ -27,24 +25,18 @@ namespace Duplicat
         {
             if (files.Any() == false) return Enumerable.Empty<IEnumerable<string>>();
 
-            // Split into those that match against the first element,
-            // and those that don't.
+            // Split into those that match against the first element, and those that don't.
             var matchesFirst = files // TODO: Cache first, don't compare against self
-                //.GroupBy(f => StreamComparison(files.First(), f));
                 .ToLookup(f => StreamComparison(files.First(), f));
 
-            //return GetContentDuplicates(matchesFirst.Single(f => f.Key == false)).Prepend(matchesFirst.Single(f => f.Key == true));
             return GetContentDuplicates(matchesFirst[false]).Prepend(matchesFirst[true]); // TODO: Find a non-recursive solution
         }
-
 
         private bool StreamComparison(string filePath1, string filePath2)
         {
             using var stream1 = _openFile(filePath1);
             using var stream2 = _openFile(filePath2);
             return stream1.AsEnumerable().SequenceEqual(stream2.AsEnumerable());
-        }
-
-        
+        }    
     }
 }
