76ab41582e7e827ce2c307f3ad51e78f3f925d01
Nick Darvey <nickdarvey@outlook.com.au>
2019-06-28 14:43:53 +1000

    Adds unit tests

---
+++ Source/Duplicat/DuplicateFinder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Duplicat
+{
+    public class DuplicateFinder
+    {
+        private readonly Func<string, Stream> _openFile;
+
+        public DuplicateFinder(Func<string, Stream> openFile) =>
+            _openFile = openFile;
+
+        public IEnumerable<IEnumerable<string>> Find(IEnumerable<(string Path, int Size)> files) =>
+            files.Select(_ => files.Select(f => f.Path));
+            //files.Select(f => new[] { f.Path });
+            //throw new NotImplementedException();
+    }
+}
---
+++ Tests/Duplicat.UnitTests/DuplicateFinderTests.cs
@@ -0,0 +1,53 @@
+﻿using FsCheck;
+using FsCheck.Xunit;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Duplicat.UnitTests
+{
+    public class DuplicateFinderTests
+    {
+        [Property]
+        public Property Returned_groups_are_not_empty(Dictionary<string, byte[]> files) =>
+            new DuplicateFinder(CreateFileOpener(files))
+                .Find(files.Select(f => (f.Key, f.Value.Length)))
+                .All(group => group.Any())
+                .ToProperty();
+
+        [Property]
+        public Property Returned_groups_where_all_elements_have_equal_content(Dictionary<string, byte[]> files) =>
+            new DuplicateFinder(CreateFileOpener(files))
+                .Find(files.Select(f => (f.Key, f.Value.Length)))
+                .All(group => group.Select(path => files[path]).Distinct(ByteArrayEqualityComparer.Instance).Count() <= 1)
+                .ToProperty();
+
+        #region Utilities
+
+        private class ByteArrayEqualityComparer : IEqualityComparer<byte[]>
+        {
+            public static ByteArrayEqualityComparer Instance { get; } = new ByteArrayEqualityComparer();
+
+            public bool Equals(byte[] x, byte[] y) => x.SequenceEqual(y);
+
+            public int GetHashCode(byte[] obj)
+            {
+                // HashCode based on contents
+                // https://stackoverflow.com/a/3329582
+                var result = 13 * obj.Length;
+                for (var i = 0; i < obj.Length; i++)
+                    result = (17 * result) + obj[i];
+                return result;
+            }
+        }
+
+        private static Func<string, Stream> CreateFileOpener(IReadOnlyDictionary<string, byte[]> files) =>
+            path => new MemoryStream(files[path]);
+
+        #endregion
+    }
+}
