9b72ff4aee7d5eeaeb9a8762c2ae350dc1da8cbb
Nick Darvey <nickdarvey@outlook.com.au>
2019-06-29 14:57:40 +1000

    Adds help and argument parsing for CLI

--- Source/Duplicat.Cli/Program.cs
+++ Source/Duplicat.Cli/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
 
 namespace Duplicat.Cli
 {
@@ -6,22 +7,47 @@ namespace Duplicat.Cli
     {
         static void Main(string[] args)
         {
-            var isSuccess = LocalDuplicateFinder.TryFind(args[0], true, out var results, out var errors);
+            var app = new CommandLineApplication();
+            app.Name = "Duplicat";
+            app.Description = "Finds duplicate files in a directory and outputs their path.";
 
-            if (isSuccess)
+            app.HelpOption("-?|-h|--help");
+
+            var pathOption = app.Option(
+                template: "-p|--path",
+                description: "The path to the directory in which to search for duplicates.",
+                CommandOptionType.SingleValue);
+
+            app.OnExecute(() =>
             {
-                foreach (var group in results)
+                if (pathOption.HasValue())
                 {
-                    foreach (var file in group) Console.WriteLine(file);
-                    Console.WriteLine();
+                    var isSuccess = LocalDuplicateFinder.TryFind(pathOption.Value(), true, out var results, out var errors);
+
+                    if (isSuccess)
+                    {
+                        foreach (var group in results)
+                        {
+                            foreach (var file in group) Console.WriteLine(file);
+                            Console.WriteLine();
+                        }
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        foreach (var error in errors) Console.Error.WriteLine(error);
+                        Console.ResetColor();
+                        return 1;
+                    }
                 }
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                foreach (var error in errors) Console.Error.WriteLine(error);
-                Console.ResetColor();
-            }
+                else
+                {
+                    app.ShowHint();
+                }
+                return 0;
+            });
+
+            app.Execute(args);
         }
     }
 }
